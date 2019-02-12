76b860d74817413a5a45781b7a484b631917e1e3
Fandy Pradana <pradana.fandy@gmail.com>
2019-02-12 11:10:54 +0700

    publishing preparation

--- sw.js
+++ sw.js
@@ -1,5 +1,582 @@
 /* global workbox:false */
 
-self.importScripts('/assets/vendor/{fileName}')
+self.importScripts('/assets/vendor/workbox-sw.js')
 
-workbox.precaching.precacheAndRoute([])
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "assets/img/admin.jpg",
+    "revision": "b606bce91f3c5e50fe06abc9ef186344"
+  },
+  {
+    "url": "assets/img/albert-dera-397063-unsplash.jpg",
+    "revision": "e9e28c1ea1230a420035f4d5c1c7f184"
+  },
+  {
+    "url": "assets/img/anthony-delanoix-8509-unsplash.jpg",
+    "revision": "e26cc90d9f36ebd6bd42c7b7d3f745bd"
+  },
+  {
+    "url": "assets/img/brandon-holmes-199535-unsplash.jpg",
+    "revision": "1a492af24c0320ef7b8e3730df9095c6"
+  },
+  {
+    "url": "assets/img/david-rodrigo-470187-unsplash.jpg",
+    "revision": "8d873c1b1614b0da7a2da85b31ebc15b"
+  },
+  {
+    "url": "assets/img/f1.jpg",
+    "revision": "9f17651c723592d087c295de59a24b19"
+  },
+  {
+    "url": "assets/img/facebook.png",
+    "revision": "c541960fbc49644850fb5bac7ac1964a"
+  },
+  {
+    "url": "assets/img/favicons/apple-touch-icon-114x114.png",
+    "revision": "129a9bbfcf6311d1e6f9ee4334c04a74"
+  },
+  {
+    "url": "assets/img/favicons/apple-touch-icon-120x120.png",
+    "revision": "6988a40e36ecff6b3f2e1ed47c249732"
+  },
+  {
+    "url": "assets/img/favicons/apple-touch-icon-144x144.png",
+    "revision": "79e1a9726dc62653509a9b821b5c7f85"
+  },
+  {
+    "url": "assets/img/favicons/apple-touch-icon-152x152.png",
+    "revision": "5d9c7a9e3305f4c47433d3f5ce6028b4"
+  },
+  {
+    "url": "assets/img/favicons/apple-touch-icon-57x57.png",
+    "revision": "2dc5caec2437a1932108c25a6da31217"
+  },
+  {
+    "url": "assets/img/favicons/apple-touch-icon-60x60.png",
+    "revision": "aacc9e0e54c66d57a47df60fbb956aa0"
+  },
+  {
+    "url": "assets/img/favicons/apple-touch-icon-72x72.png",
+    "revision": "6a31990331ea5262a6e16e8380908c56"
+  },
+  {
+    "url": "assets/img/favicons/apple-touch-icon-76x76.png",
+    "revision": "c72beb76ac50b26e0d110ad28b9c019d"
+  },
+  {
+    "url": "assets/img/favicons/favicon-128.png",
+    "revision": "de70db50730c4fbc9b36aec9201d6aa1"
+  },
+  {
+    "url": "assets/img/favicons/favicon-16x16.png",
+    "revision": "d590edb7762b431ff43d432d0794eed9"
+  },
+  {
+    "url": "assets/img/favicons/favicon-196x196.png",
+    "revision": "2fbfe3ae3520aaa51864b91e2a87e412"
+  },
+  {
+    "url": "assets/img/favicons/favicon-32x32.png",
+    "revision": "8f79c6863048f10b3568e66004fb4082"
+  },
+  {
+    "url": "assets/img/favicons/favicon-96x96.png",
+    "revision": "1414daacab4c16aa9b7442922ca2f73c"
+  },
+  {
+    "url": "assets/img/favicons/mstile-144x144.png",
+    "revision": "79e1a9726dc62653509a9b821b5c7f85"
+  },
+  {
+    "url": "assets/img/favicons/mstile-150x150.png",
+    "revision": "5ae18d48b29dec7b3c947ab2662a2dbf"
+  },
+  {
+    "url": "assets/img/favicons/mstile-310x150.png",
+    "revision": "f8302ad43d2466b73744ff5f58912bdb"
+  },
+  {
+    "url": "assets/img/favicons/mstile-310x310.png",
+    "revision": "be1b01debfc080b6418ccbaa9748df77"
+  },
+  {
+    "url": "assets/img/favicons/mstile-70x70.png",
+    "revision": "de70db50730c4fbc9b36aec9201d6aa1"
+  },
+  {
+    "url": "assets/img/general-about-us-1.jpg",
+    "revision": "4a3f770fd5132afbb8443d4e4a67838a"
+  },
+  {
+    "url": "assets/img/general-about-us-2.jpg",
+    "revision": "691c58c55d1d5607a7c6043c4ee28a66"
+  },
+  {
+    "url": "assets/img/general-faq-1.jpg",
+    "revision": "f55c1a47a02b181abc54edc8db0a5ad3"
+  },
+  {
+    "url": "assets/img/general-faq-2.jpg",
+    "revision": "41f76fe878be4b7b32b740b2eda16abd"
+  },
+  {
+    "url": "assets/img/general-home-2.jpg",
+    "revision": "b2e43b64c3154770734a8555349bfadd"
+  },
+  {
+    "url": "assets/img/general-home-3.jpg",
+    "revision": "8074f595a8e401f02a658b4491a77ef9"
+  },
+  {
+    "url": "assets/img/general-home-4-mobile.jpg",
+    "revision": "7fa0d6f4253bb68d90c7cd2d0ff5cce5"
+  },
+  {
+    "url": "assets/img/general-home-4.jpg",
+    "revision": "eecb225423e76ff76a413d6aab76fb96"
+  },
+  {
+    "url": "assets/img/general-home-5.jpg",
+    "revision": "f0baba56386e236008ce75eeb00f6ac4"
+  },
+  {
+    "url": "assets/img/general-our-team-1.jpg",
+    "revision": "f1009e89769891f3bf2b03e4ed9a03a9"
+  },
+  {
+    "url": "assets/img/general-portfolio-1.jpg",
+    "revision": "d93c15f3ee9fb8ff2ac44dbbfdfeac97"
+  },
+  {
+    "url": "assets/img/general-portfolio-2.jpg",
+    "revision": "9c623eaa2ca8878af2bf130b9102aaa7"
+  },
+  {
+    "url": "assets/img/general-pricing-1.jpg",
+    "revision": "6e203a9e3b20c51f2e6adb95911a856a"
+  },
+  {
+    "url": "assets/img/general-pricing-2.jpg",
+    "revision": "bc7ef4e5d8df51fb265e6b057bea3daa"
+  },
+  {
+    "url": "assets/img/general-service-1.jpg",
+    "revision": "6f10f8241955ea4c74487578c85422ed"
+  },
+  {
+    "url": "assets/img/general-service-2.jpg",
+    "revision": "a242c71f70923339a61dbd78a7bb3073"
+  },
+  {
+    "url": "assets/img/general.jpg",
+    "revision": "a5b1adc3bdbb3780f14a83a7556400be"
+  },
+  {
+    "url": "assets/img/google.png",
+    "revision": "54b30123af76bc6fe7759b3498002cb0"
+  },
+  {
+    "url": "assets/img/igor-ovsyannykov-211542-unsplash.jpg",
+    "revision": "5de56037c074dff9711f49afe4ea006a"
+  },
+  {
+    "url": "assets/img/jess-watters-559478-unsplash.jpg",
+    "revision": "fa4d48a4d563aca9e71cab58ae7d7ca7"
+  },
+  {
+    "url": "assets/img/joshua-fuller-285070-unsplash.jpg",
+    "revision": "e9aeeb1b8f3925f2d634a0a26d615523"
+  },
+  {
+    "url": "assets/img/m1.jpg",
+    "revision": "6698f6dc8fde1754ddbaf78d9bc24647"
+  },
+  {
+    "url": "assets/img/m2.jpg",
+    "revision": "f9a7badb451d5b20c5a64b33afc7caa8"
+  },
+  {
+    "url": "assets/img/masaaki-komori-597700-unsplash.jpg",
+    "revision": "feedb40eb88b75c82d8bdfd893f41fff"
+  },
+  {
+    "url": "assets/img/matese-fields-233175-unsplash.jpg",
+    "revision": "d33b10d04eaf060c0798991ea7758e3e"
+  },
+  {
+    "url": "assets/img/microsoft.png",
+    "revision": "c117a0bda103aeb25c145a71b0b8ac5a"
+  },
+  {
+    "url": "assets/img/oscar-sutton-452242-unsplash.jpg",
+    "revision": "408fbcce0cde9704a26d72e676f52d74"
+  },
+  {
+    "url": "assets/img/pastel.jpg",
+    "revision": "51d659b7a30f1c909d4ce669e4d96b32"
+  },
+  {
+    "url": "assets/img/rawpixel-com-274860-unsplash-1.jpg",
+    "revision": "b00741113c76cdd6ab708c7a8aa8eec9"
+  },
+  {
+    "url": "assets/img/rawpixel-com-274860-unsplash-2.jpg",
+    "revision": "69e3fb0d16b8c7d0560e2c1bc6bfc62e"
+  },
+  {
+    "url": "assets/img/rawpixel-com-274860-unsplash-3.jpg",
+    "revision": "d2cecf193412e06198948c470c398343"
+  },
+  {
+    "url": "assets/img/rawpixel-com-274860-unsplash.jpg",
+    "revision": "5a89d4f7ca86500469a9d5d85ccd9db1"
+  },
+  {
+    "url": "assets/img/space.png",
+    "revision": "a7aecbe08130c49f7235c1b1d0fe2678"
+  },
+  {
+    "url": "assets/img/spencer-imbrock-496485-unsplash.jpg",
+    "revision": "2261101f490bc44bb6130eeda2a26f36"
+  },
+  {
+    "url": "assets/img/technoart-text.png",
+    "revision": "8cc4003870ed06985a8abc313727435a"
+  },
+  {
+    "url": "assets/img/technoart.png",
+    "revision": "8c2c74763b5a2e0dd3ef0f50ce6849b0"
+  },
+  {
+    "url": "assets/img/teddy-petrosky-430688-unsplash.jpg",
+    "revision": "707a5a0f6aee7f0ee23984152eeccaec"
+  },
+  {
+    "url": "assets/img/thomas-vimare-111199-unsplash.jpg",
+    "revision": "b0da63ebd9ab228c4336d68a8aad85bd"
+  },
+  {
+    "url": "assets/img/tom-barrett-525188-unsplash.jpg",
+    "revision": "318d724fd4538a27cb91fbb76d963224"
+  },
+  {
+    "url": "assets/js/docs.min.js",
+    "revision": "a3fb6067d1351998ea5434e7d5ec7ad6"
+  },
+  {
+    "url": "assets/js/src/application.js",
+    "revision": "1878024d490c86280a92eaf7629e49dc"
+  },
+  {
+    "url": "assets/js/src/pwa.js",
+    "revision": "48f460cd4d1081a37afa962d26530617"
+  },
+  {
+    "url": "assets/vendor/anchorjs/anchor.min.js",
+    "revision": "01e6254e9f69c0c00f05060b0e1990fc"
+  },
+  {
+    "url": "assets/vendor/chart.js/Chart.bundle.min.js",
+    "revision": "88a41ecd6743bfa85e37f4d936447054"
+  },
+  {
+    "url": "assets/vendor/clipboard.js/clipboard.min.js",
+    "revision": "3f3688138a1b9fc4ef669ce9056b6674"
+  },
+  {
+    "url": "assets/vendor/font-awesome/css/font-awesome.css",
+    "revision": "c495654869785bc3df60216616814ad1"
+  },
+  {
+    "url": "assets/vendor/font-awesome/css/font-awesome.min.css",
+    "revision": "269550530cc127b6aa5a35925a7de6ce"
+  },
+  {
+    "url": "assets/vendor/font-awesome/fonts/fontawesome-webfont.svg",
+    "revision": "912ec66d7572ff821749319396470bde"
+  },
+  {
+    "url": "assets/vendor/rainbow/css/dark.css",
+    "revision": "5a7506c994bf2a50a1b3ff7037ee886f"
+  },
+  {
+    "url": "assets/vendor/rainbow/css/light.css",
+    "revision": "c5c2751a3e66c2f41db7724c5ec25328"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/c.js",
+    "revision": "9a3f98888ce900910fa92299db90a99b"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/coffeescript.js",
+    "revision": "7eb5b56c3c74e9e1a3d9049cd021464c"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/csharp.js",
+    "revision": "b20db477c1e28d1447a49653880d66ce"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/css.js",
+    "revision": "2b85c51ad8681010728d70f5da2a2ecf"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/d.js",
+    "revision": "a9ea2ad9f092a37343a7944fd7ea6fab"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/generic.js",
+    "revision": "9d1b578f497a1c625b348df76a749cb8"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/go.js",
+    "revision": "387f67dc751b4de0e512ede7372f1fd0"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/haskell.js",
+    "revision": "3f6a0509a925d39d0d35c1e9e8481ef6"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/html.js",
+    "revision": "f61a1db3a7f269d6f63195206473d2d8"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/java.js",
+    "revision": "8ab958dfbe3540d44b03f9a50b0792af"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/javascript.js",
+    "revision": "cc2728721997d2663ccb6dc0ee978b24"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/lua.js",
+    "revision": "73652f99b4a7f709a663d2759f49c299"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/php.js",
+    "revision": "b2945771993cf9f8fbc354e0ceedbaa0"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/python.js",
+    "revision": "2b0a4d1e2f386a9ea028613aae88fa07"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/r.js",
+    "revision": "02c9d1e2ad0dc0374edd59b8131436b5"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/ruby.js",
+    "revision": "36d895a594eac4164162fdde8ecf7003"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/scheme.js",
+    "revision": "7710ff810e4d673aa6d7ec1b03e73bc2"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/shell.js",
+    "revision": "72aebb651c23a5d520086376eb9976ce"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/language/smalltalk.js",
+    "revision": "290a4018add221d667c621f4339b1291"
+  },
+  {
+    "url": "assets/vendor/rainbow/js/rainbow.min.js",
+    "revision": "1576c21592c8834d7c2d3314998ccf9b"
+  },
+  {
+    "url": "assets/vendor/syntax/syntax.css",
+    "revision": "bb5ae2810b7bed63da1e9d0604db691c"
+  },
+  {
+    "url": "assets/vendor/workbox-sw.js",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  },
+  {
+    "url": "dist/css/technoart.css",
+    "revision": "821ab44f783b6b7fc0dc6322bfaf172d"
+  },
+  {
+    "url": "dist/css/technoart.min.css",
+    "revision": "9266e409a280d0c0405731ed28a1ff59"
+  },
+  {
+    "url": "dist/js/technoart.js",
+    "revision": "af9f1087e450cd0f38e7b098da4242ed"
+  },
+  {
+    "url": "dist/js/technoart.min.js",
+    "revision": "ef4e11112eab07fd9cfc57ea2d79a63e"
+  },
+  {
+    "url": "guide/index.html",
+    "revision": "1d38662723935e988051d4d38979febf"
+  },
+  {
+    "url": "id/guide/index.html",
+    "revision": "11277af006e715cc0417437411bf3fe0"
+  },
+  {
+    "url": "id/index.html",
+    "revision": "649d37ae89791af59bcf12910c15f455"
+  },
+  {
+    "url": "id/starter-template/admin/index.html",
+    "revision": "add63b89927f46ffc971ebbee519af92"
+  },
+  {
+    "url": "id/starter-template/general/about-us-1/index.html",
+    "revision": "388a299870153d15a91160e312148900"
+  },
+  {
+    "url": "id/starter-template/general/about-us-2/index.html",
+    "revision": "b2a56dd1ca8516fe718ac6afe3e67da7"
+  },
+  {
+    "url": "id/starter-template/general/cheatsheet/index.html",
+    "revision": "eeae7a049224214612164fac551a14ca"
+  },
+  {
+    "url": "id/starter-template/general/faq-1/index.html",
+    "revision": "67bc6bd739801e9e27a637420a65f45a"
+  },
+  {
+    "url": "id/starter-template/general/faq-2/index.html",
+    "revision": "ba6c0797892fd147b8d6f9a2896e379b"
+  },
+  {
+    "url": "id/starter-template/general/home-2/index.html",
+    "revision": "86daa9b30cda0fe34a4e3f8c1dc3540f"
+  },
+  {
+    "url": "id/starter-template/general/home-3/index.html",
+    "revision": "d260cb73ea3eaf81acf98421974bad83"
+  },
+  {
+    "url": "id/starter-template/general/home-4/index.html",
+    "revision": "e867df8d3a43b60022d1d0f0ace453f4"
+  },
+  {
+    "url": "id/starter-template/general/home-5/index.html",
+    "revision": "3a0ae6ab6570791c02dd83a21f2b48cf"
+  },
+  {
+    "url": "id/starter-template/general/index.html",
+    "revision": "63d12a6851a9b777dc2fc33a30fc885d"
+  },
+  {
+    "url": "id/starter-template/general/our-team-1/index.html",
+    "revision": "7af73f71128d031add79483b708abdbd"
+  },
+  {
+    "url": "id/starter-template/general/portfolio-1/index.html",
+    "revision": "fe4b7e97c608326669e360058d2c7b0d"
+  },
+  {
+    "url": "id/starter-template/general/portfolio-2/index.html",
+    "revision": "23b1dcc5c6d4641d255c74618e03f73e"
+  },
+  {
+    "url": "id/starter-template/general/pricing-1/index.html",
+    "revision": "cf62eb8dd88d32995c98265b376ccdce"
+  },
+  {
+    "url": "id/starter-template/general/pricing-2/index.html",
+    "revision": "eef1fedc432f02661dac4006f5f78492"
+  },
+  {
+    "url": "id/starter-template/general/service-1/index.html",
+    "revision": "64ca9291f4184ce3f8ca7b91bd08d073"
+  },
+  {
+    "url": "id/starter-template/general/service-2/index.html",
+    "revision": "631f9b54b0948807f4dffdee151a97fa"
+  },
+  {
+    "url": "id/starter-template/index.html",
+    "revision": "308fdc65ca8ab68346e44a68875cc366"
+  },
+  {
+    "url": "index.html",
+    "revision": "8b38e1c367f96ae9b76f47cb31414647"
+  },
+  {
+    "url": "redirects.json",
+    "revision": "99914b932bd37a50b983c5e7c90ae93b"
+  },
+  {
+    "url": "starter-template/admin/index.html",
+    "revision": "0f2b93aee9e136eb2d5a495db02d6f6e"
+  },
+  {
+    "url": "starter-template/general/about-us-1/index.html",
+    "revision": "359366fc40e06ca7a00c6ee35a165d59"
+  },
+  {
+    "url": "starter-template/general/about-us-2/index.html",
+    "revision": "3e7280835e3e5cb8f422fe6a4307b0c4"
+  },
+  {
+    "url": "starter-template/general/cheatsheet/index.html",
+    "revision": "ba08f11670426c8aeb233d4d04a4975f"
+  },
+  {
+    "url": "starter-template/general/faq-1/index.html",
+    "revision": "586940b7a0e9a6dc191cb3be32d2f696"
+  },
+  {
+    "url": "starter-template/general/faq-2/index.html",
+    "revision": "c3716277e9852c3bd1d81ef7674d753f"
+  },
+  {
+    "url": "starter-template/general/home-2/index.html",
+    "revision": "409adcc790b5eb2236260306036c467a"
+  },
+  {
+    "url": "starter-template/general/home-3/index.html",
+    "revision": "7eff501ad4291f34922c0b7c13c3212c"
+  },
+  {
+    "url": "starter-template/general/home-4/index.html",
+    "revision": "afeef98849855f863944624e5eab442f"
+  },
+  {
+    "url": "starter-template/general/home-5/index.html",
+    "revision": "17d81c21dce4a0b29bc4385f4169cdae"
+  },
+  {
+    "url": "starter-template/general/index.html",
+    "revision": "0f7c4ba172c443c42eef0f1b69416838"
+  },
+  {
+    "url": "starter-template/general/our-team-1/index.html",
+    "revision": "bf95482ada3dfb4c8a8ade961551c2dd"
+  },
+  {
+    "url": "starter-template/general/portfolio-1/index.html",
+    "revision": "2ea41cd34f2f135ef6e2fb6af58692d6"
+  },
+  {
+    "url": "starter-template/general/portfolio-2/index.html",
+    "revision": "670032d99d36bd93cf0782ca7797ee4a"
+  },
+  {
+    "url": "starter-template/general/pricing-1/index.html",
+    "revision": "41f9f10991b8122c1e9a32f68b4b6fe9"
+  },
+  {
+    "url": "starter-template/general/pricing-2/index.html",
+    "revision": "0b2371cc93a64ac41994fa163625f60f"
+  },
+  {
+    "url": "starter-template/general/service-1/index.html",
+    "revision": "2096eb7c5a0494d554302b4133fb84ec"
+  },
+  {
+    "url": "starter-template/general/service-2/index.html",
+    "revision": "7135e0eae9a6f3ecf6c367f35be34976"
+  },
+  {
+    "url": "starter-template/index.html",
+    "revision": "1b06f57f3fbd67a3642ff873532203e6"
+  }
+])
